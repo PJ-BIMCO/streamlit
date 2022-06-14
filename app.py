f0fe496efddb8b8c61cb6bf349466eab66d98e26
PJ-BIMCO <pch@bimco.org>
2022-06-14 16:04:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
-
-from bokeh.plotting import figure
+import matplotlib.pyplot as plt
 
 
 df = pd.read_pickle('MoU.pkl')
@@ -57,17 +56,8 @@ d = st.date_input(
 
 
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
-
-p.line(x, y, legend_label='Trend', line_width=2)
-
-st.bokeh_chart(p, use_container_width=True)
-
+st.pyplot(fig)
