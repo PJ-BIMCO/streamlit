06000341f4692ba9bf709898ba25a01d138e0299
PJ-BIMCO <pch@bimco.org>
2022-06-14 14:17:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_icon="📈", page_title="BIMCO Test")
+st.set_page_config(page_icon="BIMCO_Logo.png", page_title="MoU BIMCO")
 
 st.sidebar.image(
     "BIMCO_Logo.png",
-    width=50,
+    width=500,
 )
