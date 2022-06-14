7bf8711bcd141986d35f003202c9f3b4a6349ff2
PJ-BIMCO <pch@bimco.org>
2022-06-14 14:21:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.set_page_config(page_icon="BIMCO_Logo_small.png", page_title="MoU BIMCO")
 
@@ -10,8 +11,8 @@ st.sidebar.image(
 
 
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
