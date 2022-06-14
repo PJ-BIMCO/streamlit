ef5efd6a2bb595b104684a253cb0f5451f464a2f
PJ-BIMCO <pch@bimco.org>
2022-06-14 16:02:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import datetime
 
+from bokeh.plotting import figure
+
+
 df = pd.read_pickle('MoU.pkl')
 
 
@@ -52,4 +55,19 @@ d = st.date_input(
      "To:",
      datetime.date(2019, 7, 6))
 
+
+
+
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+     title='simple line example',
+     x_axis_label='x',
+     y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
 
