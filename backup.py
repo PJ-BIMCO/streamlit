f4707e50e293d61ede498a7e8b129bc1f7bb8b18
PJ-BIMCO <pch@bimco.org>
2022-06-20 16:18:33 +0200

    Update backup.py

--- backup.py
+++ backup.py
@@ -3,8 +3,15 @@ import pandas as pd
 import numpy as np
 
 
-st.set_page_config(page_icon="BIMCO_Logo_small.png", page_title="MoU BIMCO",layout="wide")
+st.set_page_config(page_icon="BIMCO_Logo_small.png", page_title="MoU BIMCO")
 
 
 shipTypeWeighted = pd.read_pickle('shipTypeWeighted.pkl')
+
+
+st.markdown("Weekly News")
+
+
+
+st.dataframe(shipTypeWeighted)
 
