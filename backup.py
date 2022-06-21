550a4980930e7477c7292b1b6ea6927790fce587
PJ-BIMCO <pch@bimco.org>
2022-06-21 17:11:57 +0200

    Update backup.py

--- backup.py
+++ backup.py
@@ -112,7 +112,7 @@ st.image(
     width=200,
 )
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+selected_page = st.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
 
 
