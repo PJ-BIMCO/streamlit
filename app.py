e84c547219c8f2c1ad64c64870c68c07210b9468
PJ-BIMCO <pch@bimco.org>
2022-06-21 09:57:22 +0200

    Added pages

--- app.py
+++ app.py
@@ -3,6 +3,24 @@ import pandas as pd
 import numpy as np
 
 
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
 
 df = pd.read_pickle('MoU.pkl')
 
@@ -10,8 +28,11 @@ shipTypeMonth = pd.read_pickle('shipTypeMonth.pkl')
 shipTypeTotal = pd.read_pickle('shipTypeTotal.pkl')
 shipTypeWeighted = pd.read_pickle('shipTypeWeighted.pkl')
 
-
 st.set_page_config(page_icon="BIMCO_Logo_small.png", page_title="MoU BIMCO",layout="wide")
+
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
 
 
 st.image(
