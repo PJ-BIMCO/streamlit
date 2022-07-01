a7bbc839c03d34db8cbf3def406bd10a3df86d25
PJ-BIMCO <pch@bimco.org>
2022-07-01 15:06:34 +0200

    Update backup.py

--- backup.py
+++ backup.py
@@ -223,9 +223,9 @@ def page6():
         test = pd.DataFrame.from_dict([weightedDict])
 
         test = test.T
-        #test.columns = ['n']
+        test.columns = ['n']
 
-        #test = test.sort_values(by='n', ascending=False)
+        test = test.sort_values(by='n', ascending=False)
 
 
         st.markdown(test)       
